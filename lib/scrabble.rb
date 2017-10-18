6bbf79b68065642a61661eba71c9fdc3cf765c81
TMad <madsen.tyler@gmail.com>
2017-10-18 10:51:33 -0600

    add scrabble game

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,10 +1,91 @@
+require 'pry'
+
 class Scrabble
 
+
   def score(word)
-    1
+    return 0 if word == nil
+    tally_points(word)
   end
 
-  def point_values
+  def score_letters(word)
+    letter_values = []
+    word.upcase.chars.each do |letter|
+      letter_values << POINT_VALUES[letter]
+    end
+    letter_values
+  end
+
+  def tally_points(word)
+    letter_values = score_letters(word)
+    score = 0
+    letter_values.each do |letter_value|
+      score = score += letter_value
+    end
+    score
+  end
+
+  def score_with_multipliers(word, multiplier, word_multiplier = 1)
+    letter_values = score_letters(word)
+    score = tally_multiplier(letter_values, multiplier) + seven_letter_bonus(word)
+    score * word_multiplier
+  end
+
+  def tally_multiplier(letter_values, multiplier)
+    score = 0
+    letter_values.map.with_index do |value, index|
+      score = score += (letter_values[index] * multiplier[index])
+    end
+    score
+  end
+
+  def seven_letter_bonus(word)
+    if word.length > 6
+      10
+    else
+      0
+    end
+  end
+
+  def highest_scoring_word(words)
+    word_scores = tally_each_word(words)
+    highest_score_index = find_index_of_highest_scoring_words(word_scores)
+    high_scoring_words = find_all_highest_scoring_words(highest_score_index, words)
+    find_high_score_with_lowest_tiles(high_scoring_words)
+  end
+
+  def tally_each_word(words)
+    word_scores = []
+    words.each do |word|
+      word_scores << tally_points(word)
+    end
+    word_scores
+  end
+
+  def find_index_of_highest_scoring_words(word_scores)
+    highest_score_index = []
+    word_scores.each_with_index do |score, index|
+      if score == word_scores.max
+        highest_score_index << index
+      end
+    end
+    highest_score_index
+  end
+
+  def find_all_highest_scoring_words(highest_score_index, words)
+    highest_score_index.map! do |index|
+      words[index]
+    end
+  end
+
+  def find_high_score_with_lowest_tiles(high_scoring_words)
+    high_scoring_words.min_by do |word|
+      word.length
+    end
+  end
+
+
+  POINT_VALUES =
     {
       "A"=>1, "B"=>3, "C"=>3, "D"=>2,
       "E"=>1, "F"=>4, "G"=>2, "H"=>4,
@@ -14,5 +95,5 @@ class Scrabble
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
       "Y"=>4, "Z"=>10
     }
-  end
+
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,9 +5,60 @@ require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
+
+  def test_it_exists
+    game = Scrabble.new
+
+    assert_instance_of Scrabble, game
+  end
+
   def test_it_can_score_a_single_letter
-    skip
+
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
+  end
+
+  def test_it_can_score_a_whole_word
+
+    assert_equal 8, Scrabble.new.score("hello")
+    assert_equal 8, Scrabble.new.score("SwEeT")
+  end
+
+  def test_it_can_score_an_empty_string
+    assert_equal 0, Scrabble.new.score("")
+  end
+
+  def test_it_can_score_a_nil_string
+    assert_equal 0, Scrabble.new.score(nil)
+  end
+
+  def test_it_can_score_letter_multiplier
+    game = Scrabble.new
+
+    assert_equal 9, game.score_with_multipliers('hello', [1,2,1,1,1])
+  end
+
+  def test_it_can_score_word_multiplier
+    game = Scrabble.new
+
+    assert_equal 18, game.score_with_multipliers('hello', [1,2,1,1,1], 2)
+  end
+
+  def test_it_can_score_7_letter_bonus
+    game = Scrabble.new
+
+    assert_equal 58, game.score_with_multipliers('sparkle', [1,2,1,3,1,2,1], 2)
+  end
+
+  def test_finds_highest_scoring_word
+    game = Scrabble.new
+
+    assert_equal 'home', game.highest_scoring_word(['home', 'word', 'hello', 'sound'])
+  end
+
+  def test_finds_highest_scoring_word_choose_fewest_tiles
+    game = Scrabble.new
+
+    assert_equal 'word', game.highest_scoring_word(['hello', 'word', 'sound'])
   end
 end
